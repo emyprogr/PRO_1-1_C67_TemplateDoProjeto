c44c00bb3b25085bc182c3801e46e6b4bb115632
emyprogr <119755944+emyprogr@users.noreply.github.com>
2023-11-08 20:47:20 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,14 @@
 import firebase from 'firebase';
 
 // adicione SDK do Firebase
-var firebaseConfig = {
-
+const firebaseConfig = {
+  apiKey: "AIzaSyBo86Ur4x5QJ2BPRmqTEZMXEpoUqtHvuLk",
+  authDomain: "projeto-67-29e87.firebaseapp.com",
+  projectId: "projeto-67-29e87",
+  storageBucket: "projeto-67-29e87.appspot.com",
+  messagingSenderId: "365270548969",
+  appId: "1:365270548969:web:f72a6bd9e08809e00b4b78",
+  measurementId: "G-HRDL6NJ3PN"
 };
 // Initialice o Firebase
 firebase.initializeApp(firebaseConfig);
